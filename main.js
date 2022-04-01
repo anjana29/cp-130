ba5fedea2b0ee4d4020725a5933098e810558e5b
anjana29 <80512586+anjana29@users.noreply.github.com>
2022-04-01 18:45:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+leftwristx=0;
+leftwristy=0;
+rightwristx=0;
+rightwristy=0;
+scoreleftwrist=0;
+scorerightwrist=0;
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function draw(){
+    image(video,0,0,600,500);
+    fill("#ff0000");
+    stroke("#ff0000");
+    if(scorerightwrist > 0.2){
+
+    
+    circle(rightwristx,rightwristy,20);
+    if(rightwristy > 0 && rightwristy<=100){
+        document.getElementById("speed").innerHTML="speed=0.5x";
+        song.rate(0.5);
+    }
+    else if(rightwristy>100 && rightwristy<=200){
+        document.getElementById("speed").innerHTML="speed=1x";
+        song.rate(1);
+    }
+    else if(rightwristy>200 && rightwristy<=300){
+      document.getElementById("speed").innerHTML="speed=1.5x";
+      song.rate(1.5);
+    }
+    else if(rightwristy>300 && rightwristy<=400){
+       document.getElementById("speed").innerHTML="speed=2x";
+       song.rate(2);
+    }
+    else if(rightWrist>400){
+        document.getElementById("speed").innerHTML="speed=2.5x";
+        song.rate(2.5);
+    }
+    }
+    if(scoreleftwrist>0.2){
+    circle(leftwristx,leftwristy,20);
+    innumberleftwristy=Number(leftwristy);
+    removedecimals=floor(innumberleftwristy);
+    volume=removedecimals/500;
+    document.getElementById("volume").innerHTML="volume = "+volume;
+    song.setVolume(volume);
+    }
+}
+song="";
+function preload(){
+    song=loadSound("music.mp3");
+}
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+function modelLoaded(){
+
+console.log("poseNet is initialized");
+}
+function gotPoses(results){
+if(results.length>0){
+console.log(results);
+scoreleftwrist=results[0].pose.keypoints[9].score;
+scorerightwrist=results[0].pose.keypoints[10].score;
+leftwristx=results[0].pose.leftWrist.x;
+leftwristy=results[0].pose.leftWrist.y;
+console.log("leftwristx = "+leftwristx+",leftwristy = "+leftwristy);
+rightwristx=results[0].pose.rightWrist.x;
+rightwristy=results[0].pose.rightWrist.y;
+console.log("rightwristx = "+rightwristx+",rightwristy = "+rightwristy);
+}
+
+}
